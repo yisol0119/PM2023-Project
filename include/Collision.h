7b622b1e3dabe6b2aed8c5544e8719fcb1729328
ParkSeongJin99 <150404430+ParkSeongJin99@users.noreply.github.com>
2023-11-16 17:40:48 +0900

    Update and rename Collision.h to include/Collision.h

--- include/Collision.h
+++ include/Collision.h
@@ -13,22 +13,22 @@ class Collision {
 
 public:
 
-	enum COLLISION_TYPE { NONE, ONPLAT, LEFT, RIGHT, TOP, BOTTOM }; // collisionÀÌ ¾îµğ¿¡¼­ ÀÏ¾î³µ´ÂÁö ¹İÈ¯ÇÏ´Â ENUM
-	//ONPLAT°ú TOPÀº ´Ù¸£´Ù
+	enum COLLISION_TYPE { NONE, ONPLAT, LEFT, RIGHT, TOP, BOTTOM }; // collisionì´ ì–´ë””ì—ì„œ ì¼ì–´ë‚¬ëŠ”ì§€ ë°˜í™˜í•˜ëŠ” ENUM
+	//ONPLATê³¼ TOPì€ ë‹¤ë¥´ë‹¤
 
 	void collide(Stage& stage);
 	bool isCollisionDetected(const Bubble& bub1, const Bubble& bub2);
 	bool isCollisionDetected(const Enemy& Enm,const Bubble& bub);
 	bool isCollisionDetected(const Player& ply, const Bubble& bub);
 	bool isCollisionDetected(const Player& ply, const Enemy& Enm);
-	COLLISION_TYPE isCollisionDetected(const Player& ply, const Wall& wal);   // wall, platformÀº Ãæµ¹ ¹æÇâÀÌ ´Ù¸£±â ¶§¹®¿¡ 
+	COLLISION_TYPE isCollisionDetected(const Player& ply, const Wall& wal);   // wall, platformì€ ì¶©ëŒ ë°©í–¥ì´ ë‹¤ë¥´ê¸° ë•Œë¬¸ì— 
 	COLLISION_TYPE isCollisionDetected(const Player& ply, const Platform& plat);
 	COLLISION_TYPE isCollisionDetected(const Enemy& enm, const Wall& wal);
 	COLLISION_TYPE isCollisionDetected(const Enemy& enm, const Platform& plat);
 
 	void handleCollision(Bubble& bub1, Bubble& bub2);
-	bool handleCollision(Enemy& Enm, Bubble& bub); //¹İÈ¯°ªÀº Ãæµ¹ ¿©ºÎ¸¦ ÀÇ¹Ì
-	bool handleCollision(Player& ply, Bubble& bub);//¹İÈ¯°ªÀº Ãæµ¹ ¿©ºÎ¸¦ ÀÇ¹Ì
+	bool handleCollision(Enemy& Enm, Bubble& bub); //ë°˜í™˜ê°’ì€ ì¶©ëŒ ì—¬ë¶€ë¥¼ ì˜ë¯¸
+	bool handleCollision(Player& ply, Bubble& bub);//ë°˜í™˜ê°’ì€ ì¶©ëŒ ì—¬ë¶€ë¥¼ ì˜ë¯¸
 	void handleCollision(Player& ply, Enemy& Enm);
 	void handleCollision(Player& ply, const Wall& wal);
 	void handleCollision(Player& ply, Platform& plat);
